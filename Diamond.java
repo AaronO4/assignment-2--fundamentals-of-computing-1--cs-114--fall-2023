05643ae65153ab86d7eae6a54672ffabc5eb8c85
AaronO4 <soliver@hartford.edu>
2023-10-05 23:26:43 -0400

    ??
    no clue what i did lol. pushing this to open vscode

--- Diamond.java
+++ Diamond.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
-    // make sure users are putting in ints
+    //variable graveyard
+    int diamondSize;
+    int increment = 0;
+
     // make sure code doesn't work for Less Than 2
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Insert size:");
+    diamondSize = scan.nextInt();
+    if (diamondSize > 2);{
+      for(diamondSize%2==1;);{
+        System.out.println("+");
+        increment++;
+      }
+    }
+    scan.close();
+
   }
 }
