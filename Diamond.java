de2431dfc27d6b18e1f8d811bf104235d9bc1a6e
AaronO4 <soliver@hartford.edu>
2023-10-09 04:03:05 -0400

    loops, stacks

--- Diamond.java
+++ Diamond.java
@@ -4,22 +4,28 @@ public class Diamond {
   public static void main(String[] args) {
     //variable graveyard
     int diamondSize;
-
+int diamond = 1;
 
     Scanner scan = new Scanner(System.in);
     System.out.println("Insert size:");
     diamondSize = scan.nextInt();
-    if(diamondSize%2!=0 && diamondSize > 2){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
-      for(int rows = 0; diamondSize > rows; rows++){
-        for (int diamond = 1; diamond <= rows; diamond++)
-          System.out.println("+");
+
+    int halfDiamond = diamondSize/2;
+
+    if(diamondSize > 2 && diamondSize%2 != 0){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
+      for(int rows = 1; halfDiamond >= rows; ++rows){
+          for (diamond = 1; diamond <= rows; ++diamond)
+            System.out.print("+");
+          System.out.println(" ");
       }
+
     scan.close();
     }
-    else if(diamondSize > 2){ //runs even diamond sizes, makes sure diamond is big enough
-      for(int rows = 0; diamondSize > rows; rows++){
-        for (int diamond = 1; diamond <= rows; diamond++)
-          System.out.println("+");
+    if(diamondSize > 2 && diamondSize%2 == 0 ){ //runs even diamonds, makes sure diamond is big enough
+      for(int rows = 1; halfDiamond >= rows; ++rows){
+        for (diamond = 1; diamond <= rows; ++diamond)
+          System.out.print("* ");
+          System.out.println(" ");
     }
   }
 
