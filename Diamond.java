d23a09250d5657905ff59f022653c4ef60fb29b5
AaronO4 <soliver@hartford.edu>
2023-10-09 00:30:18 -0400

    ??

--- Diamond.java
+++ Diamond.java
@@ -10,17 +10,18 @@ public class Diamond {
     System.out.println("Insert size:");
     diamondSize = scan.nextInt();
     if(diamondSize%2!=0 && diamondSize > 2){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
-      for(int i = 0; diamondSize > i; i++){
-        System.out.println("+");
+      for(int rows = 0; diamondSize > rows; rows++){
+        for (int diamond = 1; diamond <= rows; diamond++)
+          System.out.println("+");
       }
     scan.close();
     }
     else if(diamondSize > 2){ //runs even diamond sizes, makes sure diamond is big enough
-      for(int i = 0; diamondSize > i; i++){
-        System.out.println("++");
+      for(int rows = 0; diamondSize > rows; rows++){
+        for (int diamond = 1; diamond <= rows; diamond++)
+          System.out.println("+");
     }
   }
-
 
   }
 }
