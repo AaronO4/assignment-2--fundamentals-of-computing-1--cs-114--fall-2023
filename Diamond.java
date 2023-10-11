f12894bcb64a6808fd1a750efe071975f7682ef9
AaronO4 <soliver@hartford.edu>
2023-10-11 15:41:24 -0400

    removed halfDiamond variable
    replaced with "asterisk/2" which does the same thing but without playing footsie with an extra int

--- Diamond.java
+++ Diamond.java
@@ -4,21 +4,19 @@ public class Diamond {
   public static void main(String[] args) {
     //variable graveyard
     int diamondSize;
-int asterisk = 1;
+    int asterisk = 1;
 
     Scanner scan = new Scanner(System.in);
     System.out.println("Insert size:");
     diamondSize = scan.nextInt();
 
-    int halfDiamond = diamondSize/2;
-
     if(diamondSize > 2 && diamondSize%2 != 0){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
-      for(int rows = 1; halfDiamond >= rows; ++rows){
+      for(int rows = 1; asterisk/2 >= rows; ++rows){
           for (asterisk = 1; asterisk <= rows; ++asterisk)
             System.out.print("*");
             System.out.println(" ");
-            for (rows = halfDiamond; rows >= 1; --rows) {
-              for (asterisk = halfDiamond; rows <= asterisk; --asterisk)
+            for (rows = asterisk/2; rows >= 1; --rows) {
+              for (asterisk = asterisk/2; rows <= asterisk; --asterisk)
               System.out.print("*");
               System.out.println(" ");
             }
@@ -26,7 +24,7 @@ int asterisk = 1;
     scan.close();
     }
     if(diamondSize > 2 && diamondSize%2 == 0 ){ //runs even diamonds, makes sure diamond is big enough
-      for(int rows = 1; halfDiamond >= rows; ++rows){
+      for(int rows = 1; asterisk/2 >= rows; ++rows){
         for (int spaces = 0; spaces <= asterisk; ++spaces)
           System.out.print(" ");
         for (asterisk = 1; asterisk <= rows; ++asterisk)
@@ -34,8 +32,8 @@ int asterisk = 1;
           System.out.println(" ");
     }
 
-     for (int rows = halfDiamond; rows >= 1; --rows) {
-      for (asterisk = halfDiamond; rows <= asterisk; --asterisk)
+     for (int rows = asterisk/2; rows >= 1; --rows) {
+      for (asterisk = asterisk/2; rows <= asterisk; --asterisk)
         System.out.print("* ");
           System.out.println(" ");
       }
