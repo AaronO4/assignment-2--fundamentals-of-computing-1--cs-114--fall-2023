9eb9c76e226bce5005c7b8c78a7ccf76ba416dd0
AaronO4 <soliver@hartford.edu>
2023-10-11 15:54:18 -0400

    changed "asterisk/2" function
    it replaced the halfDiamond variable with asterisk/2. asterisk/2 was the wrong variable. now successfully replaced by diamondSize/2, which is the right variable.

--- Diamond.java
+++ Diamond.java
@@ -5,26 +5,28 @@ public class Diamond {
     //variable graveyard
     int diamondSize;
     int asterisk = 1;
+    // int column; // HEY THIS IS YOUR CUE TO FIGURE OUT THE COLUMN THING
 
     Scanner scan = new Scanner(System.in);
     System.out.println("Insert size:");
     diamondSize = scan.nextInt();
 
     if(diamondSize > 2 && diamondSize%2 != 0){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
-      for(int rows = 1; asterisk/2 >= rows; ++rows){
+      for(int rows = 1; diamondSize/2 >= rows-1; ++rows){
           for (asterisk = 1; asterisk <= rows; ++asterisk)
             System.out.print("*");
             System.out.println(" ");
-            for (rows = asterisk/2; rows >= 1; --rows) {
-              for (asterisk = asterisk/2; rows <= asterisk; --asterisk)
-              System.out.print("*");
-              System.out.println(" ");
-            }
+      }
+            for (int rows = diamondSize/2; rows >= 1; --rows) { // decrementing
+              for (asterisk = diamondSize/2; rows <= asterisk; --asterisk)
+                System.out.print("*");
+                System.out.println(" ");
           }
     scan.close();
     }
-    if(diamondSize > 2 && diamondSize%2 == 0 ){ //runs even diamonds, makes sure diamond is big enough
-      for(int rows = 1; asterisk/2 >= rows; ++rows){
+
+    if(diamondSize > 2 && diamondSize%2 == 0){ //runs even diamonds, makes sure diamond is big enough
+      for(int rows = 1; diamondSize/2 >= rows; ++rows){
         for (int spaces = 0; spaces <= asterisk; ++spaces)
           System.out.print(" ");
         for (asterisk = 1; asterisk <= rows; ++asterisk)
@@ -32,8 +34,8 @@ public class Diamond {
           System.out.println(" ");
     }
 
-     for (int rows = asterisk/2; rows >= 1; --rows) {
-      for (asterisk = asterisk/2; rows <= asterisk; --asterisk)
+     for (int rows = diamondSize/2; rows >= 1; --rows) { // decrementing
+      for (asterisk = diamondSize/2; rows <= asterisk; --asterisk)
         System.out.print("* ");
           System.out.println(" ");
       }
