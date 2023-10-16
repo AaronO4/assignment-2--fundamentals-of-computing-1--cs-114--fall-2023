08a6a340860adfe19f5a9c95f283d05e5624ebc8
AaronO4 <soliver@hartford.edu>
2023-10-16 15:52:33 -0400

    odd tophalf finalized, even finalized?

--- Diamond.java
+++ Diamond.java
@@ -13,14 +13,18 @@ public class Diamond {
 
     if(diamondSize > 2 && diamondSize%2 != 0){ //makes sure the diamond is odd, doesn't run if diamond is smaller than 2
       for(int rows = 1; rows <= diamondSize/2 + 1; ++rows){
-        // for (int spaces = 1; spaces <= diamondSize + 1 - rows; spaces++)
-        //   System.out.print(" ");
-            for (asterisk = 0; asterisk < rows; asterisk+=2) // increases by 2 because it's odd
+        for (int spaces = 1; spaces <= diamondSize/2 + 1 - rows; spaces++)
+          System.out.print(" ");
+
+            for (asterisk = 1; asterisk <= rows*2 - 1; asterisk++) // increases by 2 because it's odd (?)
               System.out.print("*");
               System.out.println(" ");
       }
-            for (int rows = 1; diamondSize/2 >= rows; ++rows) { // decrementing
-              for (asterisk = diamondSize/2; rows <= asterisk; asterisk -= 2)
+            for (int rows = diamondSize/2; 1 <= rows; --rows) { // decrementing
+               for (int spaces = 1; spaces <= diamondSize/2 + 1 - rows; spaces++)
+                  System.out.print(" ");
+
+              for (asterisk = 1; asterisk <= rows*2 - 1; asterisk += 2)
                 System.out.print("*");
                 System.out.println(" ");
           }
@@ -29,20 +33,21 @@ public class Diamond {
 
     if(diamondSize > 2 && diamondSize%2 == 0){ //runs even diamonds, makes sure diamond is big enough
       for(int rows = 1; diamondSize/2 + 1 >= rows; ++rows){
-        for (int spaces = 1; spaces <= diamondSize + 1 - rows; spaces++)
+        for (int spaces = 1; spaces <= diamondSize/2 + 1 - rows; spaces++)
           System.out.print(" ");
 
             for (asterisk = 1; asterisk <= rows*2 - 1; asterisk += 2)
               System.out.print(" *");
               System.out.println(" ");
+      }
+              //bottom half, decrementing
+      for (int rows = diamondSize/2; 1 <= rows; --rows) {
+        for (int spaces = 1; spaces <= diamondSize/2 + 1 - rows; spaces++)
+          System.out.print(" ");
 
-      // for (rows = 1; diamondSize/2 + 1 <= rows; --rows)// decrementing
-      //   for (int spaces = 1; spaces <= diamondSize - 1 + rows; spaces--)
-      //     System.out.print(" ");
-
-      // for (asterisk = diamondSize/2; asterisk >= rows*2 - 1; asterisk--) // because its even, asterisk must decrease by 1
-      //   System.out.print(" *");
-      //   System.out.println(" ");
+      for (asterisk = 1; asterisk <= rows*2 - 1; asterisk += 2) // because its even, asterisk must decrease by 1
+        System.out.print(" *");
+        System.out.println(" ");
     }
   }
   }
